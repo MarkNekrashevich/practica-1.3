62b774109d4dd17f5a87065a42c5a24a3fe5500d
admin <admin@USER-PC>
2024-04-13 12:29:57 +0300

    Добавьте файлы проекта.

---
+++ practica 1.3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace practica_1._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(620, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать значение фукнции для примера, или рассчитать введя свое значение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "РЕЗУЛЬТАТ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(641, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 187);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ practica 1.3/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace practica_1._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double v = 0.001; // заданное значение v
+                double pi = Math.PI;
+
+                label1.Text = ""; // Очищаем текст в label1
+
+                double x1 = 5;
+                double x2 = 2 * pi / 3;
+                double x3 = pi / 6;
+
+                if (double.TryParse(textBox1.Text, out double x))
+                {
+                    double f = CalculateF(x, pi, v);
+                    label1.Text += $"Значение функции F({x}) равно {f:F4}\n";
+                }
+                else
+                {
+                    double f1 = CalculateF(x1, pi, v);
+                    double f2 = CalculateF(x2, pi, v);
+                    double f3 = CalculateF(x3, pi, v);
+
+                    label1.Text += $"Значение функции F({x1}) равно {f1:F4}\n";
+                    label1.Text += $"Значение функции F({x2}) равно {f2:F4}\n";
+                    label1.Text += $"Значение функции F({x3}) равно {f3:F4}\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private double CalculateF(double x, double pi, double v)
+        {
+            double f = 0; // Начальное значение функции F(x)
+
+            double currentTerm;
+            int n = 1; // Номер текущего члена ряда
+
+            do
+            {
+                currentTerm = Math.Sin((2 * n - 1) * x) / ((2 * n - 1) * (2 * n - 1) * (2 * n - 1));
+                f += currentTerm;
+                n++;
+            } while (Math.Abs(currentTerm) >= v);
+
+            return f * 18 / pi + Math.Sin(x); // Вычисление значения функции F(x)
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
